f23414db71978ed7d167dd1ea89acbe27024cfaf
JOSEPHTEMBU <josephmulama30@gmail.com>
2021-06-24 21:03:33 +0300

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+# from flask import Flask
+
+
+# app = Flask(__name__)
+
+# from app import views
+from flask import Flask
+from .config import DevConfig
+
+# Initializing application
+app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+
+from app import views
